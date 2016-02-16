1ee031cb137373f6d1cb73745e6d8c8f86d8a3d8
Blaise Schaeffer <schabl01@luther.edu>
2016-02-16 09:06:56 -0600

    More Additons to Calc project and creating a Dicey Project

---
+++ Dicey/Dicey/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Dicey
+//
+//  Created by Blaise Schaeffer on 2/16/16.
+//  Copyright © 2016 Blaise2s. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+}
+
