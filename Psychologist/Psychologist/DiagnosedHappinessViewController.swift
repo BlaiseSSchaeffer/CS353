cbbd18c5959fc7a8d9e3c2125aefbad448cc29cb
Blaise Schaeffer <schabl01@luther.edu>
2016-03-08 21:47:31 -0600

    Working on happiness, calculator, and psychologist

---
+++ Psychologist/Psychologist/DiagnosedHappinessViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DiagnosedHappinessViewController.swift
+//  Psychologist
+//
+//  Created by Blaise Schaeffer on 3/8/16.
+//  Copyright © 2016 Blaise2s. All rights reserved.
+//
+
+import UIKit
+
+class DignisedHappinessViewController : HappinessViewController, UIPopoverPresentationControllerDelegate {
+    override var happiness: Int {
+        didSet {
+            diagnosticHistory += [happiness]
+        }
+    }
+    
+    private let defaults = NSUserDefaults.standardUserDefaults()
+    
+    var diagnosticHistory: [Int] {
+        get {
+            return defaults.objectForKey(History.DefaultsKey) as? [Int] ?? []
+        }
+        set {
+            defaults.setObject(newValue, forKey: History.DefaultsKey)
+        }
+    }
+    
+    private struct History {
+        static let SegueIdentifier = "Show Diagnostic History"
+        static let DefaultsKey = "DiagnosedHappinessViewController.History"
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier {
+            switch identifier {
+            case History.SegueIdentifier:
+                if let tvc = segue.destinationViewController as? TextViewController {
+                    if let ppc = tvc.popoverPresentationController {
+                        ppc.delegate = self
+                    }
+                    tvc.text = "\(diagnosticHistory)"
+                }
+            default: break
+            }
+        }
+    }
+    
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+        return UIModalPresentationStyle.None
+    }
+}
---
+++ Psychologist/Psychologist/TextViewController.swift
@@ -0,0 +1,37 @@
+ //
+//  TextViewController.swift
+//  Psychologist
+//
+//  Created by Blaise Schaeffer on 3/8/16.
+//  Copyright © 2016 Blaise2s. All rights reserved.
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView! {
+        didSet {
+            textView.text = text
+        }
+    }
+    
+    var text = "" {
+        didSet {
+            textView?.text = text
+        }
+    }
+    
+    override var preferredContentSize: CGSize {
+        get {
+            if textView != nil && presentingViewController != nil {
+                return textView.sizeThatFits(presentingViewController!.view.bounds.size)
+            } else {
+                return super.preferredContentSize
+            }
+        }
+        set {
+            super.preferredContentSize = newValue
+        }
+    }
+}
