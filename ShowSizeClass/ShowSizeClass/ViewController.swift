a8fce32b443aafcab32e76b166fb0eaee0e1977a
Blaise Schaeffer <schabl01@luther.edu>
2016-03-09 13:52:42 -0600

    A bit more on cals and some more demos

---
+++ ShowSizeClass/ShowSizeClass/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  ShowSizeClass
+//
+//  Created by Blaise Schaeffer on 3/9/16.
+//  Copyright © 2016 Blaise2s. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
+
