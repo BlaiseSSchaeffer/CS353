87b8209b644c727080d9df1b108aee0724dc7e43
Blaise Schaeffer <schabl01@luther.edu>
2016-04-26 09:15:09 -0500

    Final Project Stuff

---
+++ Ride/EntryTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EntryTableViewController.swift
+//  Ride
+//
+//  Created by Blaise Schaeffer on 4/26/16.
+//  Copyright © 2016 Blaise2s. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewController: UITableViewController {
+
+}
